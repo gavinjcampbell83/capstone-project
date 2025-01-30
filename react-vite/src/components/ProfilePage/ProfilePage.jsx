26f3a5944aba5fcdbff53869c7fdfa183b32f770
gavinjcampbell83 <gavinjcampbell83@gmail.com>
2025-01-30 12:47:18 -0700

    working on followers

--- react-vite/src/components/ProfilePage/ProfilePage.jsx
+++ react-vite/src/components/ProfilePage/ProfilePage.jsx
@@ -1,1 +1,3 @@
 //profile page need to work on layout/design implementing followers
+//should pre populate inputed info from DB.
+//on click edit button when you make changes and a discard changes button.
