bab7a5246ad2876678c6f5d52d7e9e16c3f48af1
gavinjcampbell83 <gavinjcampbell83@gmail.com>
2025-02-05 09:06:08 -0700

    working on followers

--- react-vite/src/components/FollowerPage/FollowerPage.jsx
+++ react-vite/src/components/FollowerPage/FollowerPage.jsx
@@ -1,3 +1,4 @@
 //This will be the follower page need to think about layout needs layout development
 //will show pictures of who you follow as well as cruz routes
 //will show total followed and total followers
+import './FollowerPage.css'
